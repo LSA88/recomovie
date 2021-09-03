3598e53bda24a616202c4f3dec6f504d98565e38
mouseteacher <mouseteacher@naver.com>
2021-09-03 13:05:31 +0900

    21.09.03.001

--- src/main/java/com/argos/r_recommender/controller/RecommendController.java
+++ src/main/java/com/argos/r_recommender/controller/RecommendController.java
@@ -26,7 +26,7 @@ public class RecommendController {
 
 	@Resource(name = "r_analysisDAO")
 	private R_analysisDAO r_analysisDAO;
-	
+
 	@Autowired
 	private Recommend_Service recommend_service;
 	Logger logger = LoggerFactory.getLogger(RecommendController.class);
@@ -34,29 +34,29 @@ public class RecommendController {
 	@RequestMapping(value = "/recommend_item_by_user", method = RequestMethod.POST)
 	@ResponseBody
 	public Map recommend_item_by_user(@RequestBody Map<String,Object> request_map ) {
-		Map response_map = r_analysisDAO.recommend_item_by_user(request_map);   
+		Map response_map = r_analysisDAO.recommend_item_by_user(request_map);
 		return response_map;
 	}
 	@RequestMapping(value = "/insert_movie_rating", method = RequestMethod.POST)
 	@ResponseBody
 	public Map insert_movie_rationg(@RequestBody Map<String,Object> request_map ) {
-		Map response_map = recommend_service.insert_movie_rating(request_map);   
+		Map response_map = recommend_service.insert_movie_rating(request_map);
 		return response_map;
 	}
 
 	@RequestMapping(value = "/user_movie_rating", method = RequestMethod.POST)
 	@ResponseBody
 	public Map user_movie_rating(@RequestBody Map<String,Object> request_map ) {
-		Map response_map = recommend_service.user_movie_rating((String)request_map.get("이름"));   
+		Map response_map = recommend_service.user_movie_rating((String)request_map.get("이름"));
 		return response_map;
 	}
-	
+
 	@RequestMapping(value = "/recommend_main", method = RequestMethod.GET)
 	public ModelAndView r_analysis_main(HttpServletRequest request) throws Exception {
 		ModelAndView mv = new ModelAndView("recommend_page/recommend_main");
 		return mv;
 	}
-	
 
-	
+
+
 }
